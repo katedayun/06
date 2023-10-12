61e3b440b9776b81858b0d74868d34bbf71c7232
katedayun <58518683+katedayun@users.noreply.github.com>
2023-10-12 16:06:45 +0900

    1

--- main.c
+++ main.c
@@ -3,27 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int sumTwo(int a,int b){
-
-	return a+b;
+void star()
+{
+	int i;
+	for (i=0;i<10;i++)
+	printf("*");
 }
 
-int square(int n){
-	return n*n;
-}
-
-int get_max(int x, int y){
-	if (x>y)
-		return x;
-	else 
-		return y;
-}
 
 int main(int argc, char *argv[]) {
-	int a,b,c;
-	a=sumTwo(1,2);
-	b=get_max(1,2);
-	c=square(1);
-	printf("datas are %d, %d, %d\n",a,b,c);
+	int i;
+	for (i=0;i<3;i++)
+	star();
 	
 }
