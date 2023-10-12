83d39f1c0a96bf6ee46e811f4649d72d022955c3
katedayun <58518683+katedayun@users.noreply.github.com>
2023-10-12 17:04:16 +0900

    5

--- main.c
+++ main.c
@@ -3,13 +3,40 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square(int a){
-	return a*a;
+int factorial (int n){
+	int res=1;
+	int i;
+	for(i=1;i<=n;i++)
+		res=res*i;
+	return res;
 }
 
+int combination(int n, int r){
+	int numerator, denomin1, denomin2;
+	numerator = factorial(n);
+	denomin1=factorial(r);
+	denomin2=factorial(n-r);
+	return numerator/(denomin1*denomin2);
+}
 
-int main(int argc, char *argv[]) {
-	int a=2;
-	a=square(a);
-	printf("a=%i\n",a);
+int get_integer( ){
+	int n;
+	printf("input n first and then r:");
+	//printf("input n,r");
+	//scanf("%d,%d",&n,&r);
+	scanf("%d",&n);
+	return n;
+	
+}
+
+int main(void)
+{
+	int answer;
+	int n, r;
+	printf("input n:");
+	scanf("%d",&n);
+	printf("input r:");
+	scanf("%d",&r);
+	answer = combination(n,r);
+	printf("answer is %d", answer);
 }
