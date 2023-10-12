c727397c4195dd88353c18ca3c4ecdf7518ca443
katedayun <58518683+katedayun@users.noreply.github.com>
2023-10-12 15:49:55 +0900

    2

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void func1(void){
+	int x;
+	printf("func1 x is at %p\n",&x);
+}
+
+
+int main(int argc, char *argv[]) {
+	int x;
+	printf("main x is at %p \n", &x);
+	func1();
+	
+	return 0;
+}
